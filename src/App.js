be3b7616331151ee0e4f10a1674eab0ae28b5102
kphongsa <kphongsa727@gmail.com>
2026-04-27 18:27:28 -0700

    Implemented app layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import ProductList from "./components/ProductList";
+import Cart from "./components/Cart";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container-fluid">
+          <span className="navbar-brand mb-0 h1">
+            Redux Shopping Cart
+          </span>
+        </div>
+      </nav>
+
+      <div className="container-fluid mt-4">
+        <div className="row">
+          <div className="col-md-8">
+            <ProductList />
+          </div>
+
+          <div className="col-md-4">
+            <Cart />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
